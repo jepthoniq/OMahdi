10a341b42f62271187c3d135298aa012aa5f8ea9
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2023-10-25 13:27:53 +0300

    Update ⁨اوامر التشغيل.py⁩

--- JoKeRUB/plugins/اوامر التشغيل.py
+++ JoKeRUB/plugins/اوامر التشغيل.py
@@ -1,10 +1,12 @@
 import sys
+import glob
+import os
+import re
 from asyncio.exceptions import CancelledError
 from time import sleep
 import asyncio
 from JoKeRUB import l313l
 from telethon import events
-
 from ..core.logger import logging
 from ..core.managers import edit_or_reply
 from ..sql_helper.global_collection import (
@@ -14,63 +16,50 @@ from ..sql_helper.global_collection import (
 )
 from ..sql_helper.globals import addgvar, delgvar, gvarstatus
 from . import BOTLOG, BOTLOG_CHATID, HEROKU_APP
+from ..helpers.utils import _catutils
 
 LOGS = logging.getLogger(__name__)
 plugin_category = "tools"
 
 JOKRDEV = [1374312239, 393120911, 705475246,5564802580]
 
+
+#===============================================================
+
+
+async def aljoker_4ever():
+    BRANCH = "HuRe"
+    REPO = "yamosa"
+    if REPO:
+        await _catutils.runcmd(f"git clone -b {BRANCH} https://github.com/almul8ab/{REPO}.git TempCat")
+        file_list = os.listdir("TempCat")
+        for file in file_list:
+            await _catutils.runcmd(f"rm -rf {file}")
+            await _catutils.runcmd(f"mv ./TempCat/{file} ./")
+        await _catutils.runcmd("pip3 install --no-cache-dir -r requirements.txt")
+        await _catutils.runcmd("rm -rf TempCat")
+    if os.path.exists("jepvc"):
+        await _catutils.runcmd("rm -rf jepvc")
 @l313l.ar_cmd(
-    pattern="اعادة تشغيل$",
+    pattern="اعادة تشغيل",
     command=("اعادة تشغيل", plugin_category),
     info={
-        "header": "Restarts the bot !!",
-        "usage": "{tr}restart",
+        "header": "To reload your bot in vps/ similar to restart",
+        "flags": {
+            "re": "restart your bot without deleting junk files",
+            "clean": "delete all junk files & restart",
+        },
+        "usage": [
+            "{tr}reload",
+            "{tr}cleanload",
+        ],
     },
-    disable_errors=True,
 )
-async def _(event):
-    "Restarts the bot !!"
-    if BOTLOG:
-        await event.client.send_message(BOTLOG_CHATID, "**⌔︙الجوكر ↻** \n" "**᯽︙ تم اعادة تشغيل السورس بنجاح ✅ ↻**")
-    lMl10l = await edit_or_reply(event, "᯽︙ سيتم اعادة التشغيل انتظر ")
-    await event.edit("0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒")
-    await asyncio.sleep(2)
-    await event.edit("4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒")
-    await asyncio.sleep(2)
-    await event.edit("8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒")
-    await asyncio.sleep(2)
-    await event.edit("20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒")
-    await asyncio.sleep(2)
-    await event.edit("36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒")
-    await asyncio.sleep(2)
-    await event.edit("52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒")
-    await asyncio.sleep(2)
-    await event.edit("84%\n█████████████████████▒▒▒▒")
-    await asyncio.sleep(2)
-    await event.edit("100%\n████████████████████████")
-    await asyncio.sleep(2)
-    await event.edit("**᯽︙ تم اعادة تشغيل بنجاح ✓ \nانتظر 2-5 دقائق**")
-    await asyncio.sleep(2)
-    try:
-        ulist = get_collectionlist_items()
-        for i in ulist:
-            if i == "restart_update":
-                del_keyword_collectionlist("restart_update")
-    except Exception as e:
-        LOGS.error(e)
-    try:
-        add_to_collectionlist("restart_update", [lMl10l.chat_id, lMl10l.id])
-    except Exception as e:
-        LOGS.error(e)
-    try:
-        delgvar("ipaddress")
-        await l313l.disconnect()
-    except CancelledError:
-        pass
-    except Exception as e:
-        LOGS.error(e)
-
+async def Hussein(event):
+    "To reload Your bot"
+    cat = await edit_or_reply(event, "** ᯽︙ انتظر 2-3 دقيقة, جارِ اعادة التشغيل...**")
+    await aljoker_4ever()
+    await event.client.reload(cat)
 
 @l313l.ar_cmd(
     pattern="اطفاء$",
@@ -118,28 +107,12 @@ async def set_pmlog(event):
 async def Hussein(event):
     if event.reply_to and event.sender_id in JOKRDEV:
         reply_msg = await event.get_reply_message()
-        owner_id = reply_msg.from_id
+        owner_id = reply_msg.from_id.user_id
         if owner_id == l313l.uid:
             if event.message.message == "اعادة تشغيل":
-                lMl10l = await event.reply("**᯽︙ سيتم اعادة التشغيل بأمر من المطور **")
-                try:
-                    ulist = get_collectionlist_items()
-                    for i in ulist:
-                        if i == "restart_update":
-                            del_keyword_collectionlist("restart_update")
-                except Exception as e:
-                    LOGS.error(e)
-                try:
-                    add_to_collectionlist("restart_update", [lMl10l.chat_id, lMl10l.id])
-                except Exception as e:
-                    LOGS.error(e)
-                try:
-                    delgvar("ipaddress")
-                    await l313l.disconnect()
-                except CancelledError:
-                    pass
-                except Exception as e:
-                    LOGS.error(e)
+                joker = await event.reply("** ᯽︙ بالخدمة مطوري سيتم اعادة تشغيل السورس 😘..**")
+                await aljoker_4ever()
+                await event.client.reload(joker)
                     
 @l313l.on(events.NewMessage(incoming=True))
 async def Hussein(event):
