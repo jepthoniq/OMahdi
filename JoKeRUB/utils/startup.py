ae4a6cda9f9be46040684f15a1f96c38f853584d
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2024-02-08 15:27:45 +0300

    Update startup.py

--- JoKeRUB/utils/startup.py
+++ JoKeRUB/utils/startup.py
@@ -28,7 +28,7 @@ from ..sql_helper.globals import addgvar, delgvar, gvarstatus
 from .pluginmanager import load_module
 from .tools import create_supergroup
 LOGS = logging.getLogger("JoKeRUB")
-logging.getLogger('telethon').setLevel(logging.WARNING)
+logging.getLogger('telethon.network.mtprotosender').setLevel(logging.ERROR)
 ##Reda hands here
 cmdhr = Config.COMMAND_HAND_LER
 bot = l313l
